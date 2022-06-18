819339f22bd094ed1a2d6ff7b49a0836d1db15b3
Tejonidhii <48520766+Tejonidhii@users.noreply.github.com>
2022-06-18 13:55:40 +0530

    Add files via upload

---
+++ src/Login/Contact.js
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Contact = () => {
+
+    return (
+
+        <div class="container-xl" class="p-3 mb-2 bg-dark text-white" >
+                <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                <h2>Contact Page</h2>
+                    <h1 class="display-4">Capstone Project - E-Healthcare</h1>
+                    <p class="lead">Project  : React / Asp.Net / SqlServer</p>
+                    <p class="lead">Submitted By : R Tejonidhii</p>
+                    <p class="lead">Emaid_ID : tejonidhii@gmail.com</p>
+                    
+
+                </div>
+                </div>
+               
+                
+        </div> 
+    );
+}
+
+export default Contact;
---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import Carousel from "react-bootstrap/Carousel";
+
+const Home = () => {
+  return (
+    <div class="container-xl">
+      <div class="row">
+        <div class="col"></div>
+        
+        <div class="col"></div>
+
+        <div class="row">
+          <div class="col"></div>
+          <div class="col-8">
+
+            <p class="lead">
+                  <h5 align="center">online medicine shop</h5>
+            </p>
+            
+            
+
+
+          </div>
+          <div class="col"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
